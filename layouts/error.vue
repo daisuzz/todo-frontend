efd395afa6859797d8f9d622af170e6c726e69b7
Daisaku Suzuki <ds.suzuki217@gmail.com>
2020-02-02 14:02:25 +0900

    typescriptを導入

--- layouts/error.vue
+++ layouts/error.vue
@@ -12,33 +12,35 @@
   </v-app>
 </template>
 
-<script>
-export default {
-  layout: "empty",
-  props: {
-    error: {
-      type: Object,
-      default: null
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    layout: "empty",
+    props: {
+      error: {
+        type: Object,
+        default: null
+      }
+    },
+    data() {
+      return {
+        pageNotFound: "404 Not Found",
+        otherError: "An error occurred"
+      }
+    },
+    head() {
+      const title =
+        this.error.statusCode === 404 ? this.$data.pageNotFound : this.$data.otherError
+      return {
+        title
+      }
     }
-  },
-  data() {
-    return {
-      pageNotFound: "404 Not Found",
-      otherError: "An error occurred"
-    }
-  },
-  head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
-    }
-  }
-}
+  })
 </script>
 
 <style scoped>
-h1 {
-  font-size: 20px;
-}
+  h1 {
+    font-size: 20px;
+  }
 </style>
