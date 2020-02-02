35bb72cf49f6009ff522e62ce28a26ee337037fc
Daisaku Suzuki <ds.suzuki217@gmail.com>
2020-02-02 12:03:07 +0900

    画面遷移をVue-Routerを使うように修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-flex class="text-left">
-      <v-btn href="/create" color="#097">
+      <v-btn to="/create" color="#097">
         Create Todo
       </v-btn>
     </v-flex>
@@ -10,20 +10,20 @@
         <v-simple-table>
           <template v-slot:default>
             <thead>
-              <tr>
-                <th class="text-center">
-                  Title
-                </th>
-                <th class="text-center">
-                  Status
-                </th>
-              </tr>
+            <tr>
+              <th class="text-center">
+                Title
+              </th>
+              <th class="text-center">
+                Status
+              </th>
+            </tr>
             </thead>
             <tbody>
-              <tr v-for="item in todoList" :key="item.id">
-                <td>{{ item.title }}</td>
-                <td>{{ item.status }}</td>
-              </tr>
+            <tr v-for="item in todoList" :key="item.id">
+              <td>{{ item.title }}</td>
+              <td>{{ item.status }}</td>
+            </tr>
             </tbody>
           </template>
         </v-simple-table>
@@ -33,32 +33,32 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      todoList: [
-        {
-          title: "ジムに行く",
-          status: "UNDONE"
-        },
-        {
-          title: "朝食を食べる",
-          status: "DONE"
-        },
-        {
-          title: "昼食を食べる",
-          status: "UNDONE "
-        },
-        {
-          title: "夕食を食べる",
-          status: "UNDONE"
-        },
-        {
-          title: "本を読む",
-          status: "DONE"
-        }
-      ]
+  export default {
+    data() {
+      return {
+        todoList: [
+          {
+            title: "ジムに行く",
+            status: "UNDONE"
+          },
+          {
+            title: "朝食を食べる",
+            status: "DONE"
+          },
+          {
+            title: "昼食を食べる",
+            status: "UNDONE "
+          },
+          {
+            title: "夕食を食べる",
+            status: "UNDONE"
+          },
+          {
+            title: "本を読む",
+            status: "DONE"
+          }
+        ]
+      }
     }
   }
-}
 </script>
